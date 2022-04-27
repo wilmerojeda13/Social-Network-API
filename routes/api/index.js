f17529b816e3fca38fb868498b7a9c36172251b3
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-04-27 15:52:37 -0400

    adding add a api folders with the files required

---
+++ routes/api/index.js
@@ -0,0 +1,6 @@
+// Requiring the routes
+const router = require('express').Router;
+const userRoutes = require('./user-routes');
+const thoughtRoutes = require('./thought-routes');
+
+//Initialize app
