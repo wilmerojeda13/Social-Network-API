55db7a28cd8f4651643b7d25e8798d51e55b8f4f
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-04-27 16:02:00 -0400

    addd exporting the router

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 // Requiring the routes
-const router = require('express').Router;
+const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const thoughtRoutes = require('./thought-routes');
 
 //Initialize router app
 router.use('/users', userRoutes);
 router.use('/thoughts', thoughtRoutes);
+
+module.exports = router;
