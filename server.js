aa3bd2d153988a43677ab90a72dece2d372a320a
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-04-27 15:32:08 -0400

    add adding the connection for mongoose

--- server.js
+++ server.js
@@ -12,4 +12,13 @@ app.use(express.static('public'));
 
 
 // Initialize app routes
-app.use(rquire('./routes'))
+app.use(require('./routes'))
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social-network-api', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
