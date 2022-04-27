0ce809983c0028b40e63c6d81f89118c74624596
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-04-27 15:55:54 -0400

    add requiring the routers by initializing routeapp

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,4 +3,6 @@ const router = require('express').Router;
 const userRoutes = require('./user-routes');
 const thoughtRoutes = require('./thought-routes');
 
-//Initialize app
+//Initialize router app
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
